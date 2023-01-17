db0ec8925b0308f9f3af8f0076b38d56c5e8cd59
J-J-T-M <jonatham.junior.18@gmail.com>
2023-01-17 18:55:51 -0400

    end

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/users">Users</router-link>
   </nav>
   <router-view/>
 </template>
@@ -11,7 +12,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  margin: 0 50px;
   color: #2c3e50;
 }
 
---
+++ frontend/src/views/Users.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="users">
+    <h1>This is an users page</h1>
+    <div v-for="user in users">
+      <h3>{{ user.name }}</h3> 
+      <div v-for="post in user.posts">
+        {{ post.title }} <br>
+      </div>
+
+    </div>
+   
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        users: []
+      }
+    },
+    async created() {
+      await fetch('http://127.0.0.1:8000/api/users')
+      .then(response => response.json())
+      .then( r => {
+        this.users = r;
+      })
+    }
+  }
+</script>
